60e614237514e59fd3ff61c3f11f46ddd17eac0e
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-30 15:10:28 -0800

    Test if Turn class exists

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,2 +1,9 @@
 require './lib/turn'
 require './lib/card'
+
+RSpec.describe Turn do
+    it 'is a turn' do
+    first_turn = Turn.new("Hawaii" , Card)
+    expect(first_turn).to be_instance_of Turn
+    end
+end
