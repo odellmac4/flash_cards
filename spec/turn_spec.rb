20ac219b5cca3c2e88d3185043f554c712a48b09
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-30 15:37:04 -0800

    Return elements of a card used in an instance of a turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,8 +3,15 @@ require './lib/card'
 
 RSpec.describe Turn do
     it 'is a turn' do
-    card = Card.new("Where is Honolulu?" , "Hawaii" , :Geography)
-    first_turn = Turn.new("Hawaii" , card)
-    expect(first_turn).to be_instance_of Turn
+        card = Card.new("Where is Honolulu?" , "Hawaii" , :Geography)
+        first_turn = Turn.new("Hawaii" , card)
+        expect(first_turn).to be_instance_of Turn
     end
+
+    it 'returns the card used for the instance of a turn' do
+        card = Card.new("Where is Honolulu?" , "Hawaii" , :Geography)
+        first_turn = Turn.new("Hawaii" , card)
+        expect(first_turn.card).to eq (card)
+    end
+
 end
