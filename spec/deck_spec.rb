f741d445c84ac9b7baed193cf284343a38ca3c6a
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-30 21:57:25 -0800

    Test if Deck class exists

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,6 +2,12 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-    it 'exists' do
-      
+    it 'creates an array of cards' do
+        card_1 = Card.new("Where is Barcelona?" , "Spain" , :Geography)
+        card_2 = Card.new("Where is Beyonce from?" , "Houston" , :Music)
+        card_3 = Card.new("What is Will Smith's occupation?" , "Actor" , :Movies)
+        cards = [card_1 , card_2 , card_3]
+        deck = Deck.new(cards)
+        expect(deck).to be_instance_of Deck
     end
+end
