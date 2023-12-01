3207e0afb08c92ab42bbb15a031a3e50d2070782
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-30 20:48:48 -0800

    Test  method to

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Turn do
         expect(first_turn).to be_instance_of Turn
     end
 
-    it 'returns the card used for the instance of a turn' do
+    it 'returns the card used for an instance of a turn' do
         card = Card.new("Where is Honolulu?" , "Hawaii" , :Geography)
         first_turn = Turn.new("Hawaii" , card)
         expect(first_turn.card).to eq (card)
@@ -18,6 +18,12 @@ RSpec.describe Turn do
         card = Card.new("Where is Honolulu?" , "Hawaii" , :Geography)
         first_turn = Turn.new("Hawaii" , card)
         expect(first_turn.guess). to eq ("Hawaii")
+    end
+
+    it 'answer is correct or incorrect' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        first_turn = Turn.new("Juneau" , card)
+        expect(first_turn.correct?).to be (true)
     end
 
 end
