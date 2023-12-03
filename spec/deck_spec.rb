9d9a78572ab44511b9f78d62db66e4e43b62c97a
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-02 19:52:54 -0800

    Test how many cards are in a deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,13 +1,25 @@
 require './lib/card'
 require './lib/deck'
 
+#Use `before(:each) do` to repeat instances in the describe block
+
 RSpec.describe Deck do
-    it 'creates an array of cards' do
+    it 'is a deck' do
         card_1 = Card.new("Where is Barcelona?" , "Spain" , :Geography)
         card_2 = Card.new("Where is Beyonce from?" , "Houston" , :Music)
         card_3 = Card.new("What is Will Smith's occupation?" , "Actor" , :Movies)
         cards = [card_1 , card_2 , card_3]
         deck = Deck.new(cards)
         expect(deck).to be_instance_of Deck
+    end
+
+    it 'count of cards in a deck' do
+        card_1 = Card.new("Where is Barcelona?" , "Spain" , :Geography)
+        card_2 = Card.new("Where is Beyonce from?" , "Houston" , :Music)
+        card_3 = Card.new("What is Will Smith's occupation?" , "Actor" , :Movies)
+        cards = [card_1 , card_2 , card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.count).to eq (3)
     end
 end
