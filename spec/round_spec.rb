bc44d74d55b3603781d6f1e9cd208b0862a7114a
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-03 09:24:09 -0800

    Test if the deck method returns full deck of cards

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Round do
 
         expect(round).to be_instance_of(Round)
     end
+
+    it 'is a deck' do
+        card_1 = Card.new("Where is Barcelona?" , "Spain" , :Geography)
+        card_2 = Card.new("Where is Beyonce from?" , "Houston" , :Music)
+        card_3 = Card.new("What is Will Smith's occupation?" , "Actor" , :Movies)
+        cards = [card_1 , card_2 , card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+
+        expect(round.deck).to eq(deck)
+    end
 end
