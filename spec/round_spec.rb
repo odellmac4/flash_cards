dd92d3e32b4ed4b62434193b4aacb00c854843f2
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-03 09:19:58 -0800

    Test insatnce of the Round class

---
+++ spec/round_spec.rb
@@ -0,0 +1,17 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+    it 'is a round' do
+        card_1 = Card.new("Where is Barcelona?" , "Spain" , :Geography)
+        card_2 = Card.new("Where is Beyonce from?" , "Houston" , :Music)
+        card_3 = Card.new("What is Will Smith's occupation?" , "Actor" , :Movies)
+        cards = [card_1 , card_2 , card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+
+        expect(round).to be_instance_of(Round)
+    end
+end
