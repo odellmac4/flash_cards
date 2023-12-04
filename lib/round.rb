f16a22a1a74fa487965065e42a00f6c8c93d3b35
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-03 19:30:52 -0800

    Create take_turn method to create a new turn and add new turn to the array of turns

--- lib/round.rb
+++ lib/round.rb
@@ -10,4 +10,27 @@ class Round
     def current_card
         @deck.cards.fetch(0)
     end
+
+    def take_turn(guess)
+        @new_turn = Turn.new(guess , current_card)
+        @turns << @new_turn
+        # @deck.cards.rotate!(1)
+        @new_turn
+    end
+
+    # def number_correct
+    #     @turns.count do |turn|
+    #         turn.correct?
+    #     end
+    # end
 end
+
+#How do I make it check thru every turn and see if each is correct (iteration method)
+# for number_correct use count method and use it as iteration (count do end)
+# @new_turn.correct?
+
+# @turns.count do |turn|
+#     turn.correct?
+# end
+
+#rotate method
